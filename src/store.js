08dc59e62728abc9522bbfd2a74f76b6ee65ebb4
sabri <sabrinatwitter6@gmail.com>
2023-11-10 15:51:08 +0100

    add components

---
+++ src/store.js
@@ -0,0 +1,7 @@
+// Importa la funzione "reactive" da Vue
+import { reactive } from "vue";
+// Crea un oggetto "store" reattivo che conterrà i dati
+export const store = reactive({
+// Un array vuoto "items" che conterrà gli elementi dei dati
+  items: [],
+});
